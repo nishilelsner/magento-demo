58c0b0e5ccd77aaa7d660356a418e7a8d7875a15
nishilelsner <nishil.s@elsner.com>
2026-04-21 16:39:25 +0530

    ui and di.xml

---
+++ app/code/Webkul/BlogManager/Controller/Adminhtml/Manage/Index.php
@@ -0,0 +1,56 @@
+<?php
+namespace Webkul\BlogManager\Controller\Adminhtml\Manage;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var Magento\Backend\App\Action\Context
+     */
+    protected $context;
+
+    /**
+     * @var Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Dependency Initilization
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Provides content
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Webkul_BlogManager::manage');
+        $resultPage->getConfig()->getTitle()->prepend(__('Manage Blog'));
+        return $resultPage;
+    }
+
+    /**
+     * Check Autherization
+     *
+     * @return boolean
+     */
+    public function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Webkul_BlogManager::manage');
+    }
+}
