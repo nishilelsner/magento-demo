5199d7dcfaa41558e3ef43a29fdda71256710e5f
nishilelsner <nishil.s@elsner.com>
2026-05-05 13:05:00 +0530

    Added the modal and opened it on waitlist button click

---
+++ app/code/Practice/Waitlist/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        'waitlist_module_js': "Practice_Waitlist/js/waitlist_modal"
+    },
+    shim: {
+        'waitlist_module_js': {
+            deps: ['jquery']
+        }
+    }
+} 
---
+++ app/code/Practice/Waitlist/view/frontend/web/js/waitlist_modal.js
@@ -0,0 +1,27 @@
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function ($, modal) {
+    'use strict';
+
+    return function (config, element) {
+        var options = {
+            type: 'popup',
+            responsive: true,
+            innerScroll: true,
+            buttons: [{
+                text: $.mage.__('Continue'),
+                class: 'mymodal1',
+                click: function () {
+                    this.closeModal();
+                }
+            }]
+        };
+
+        var popup = modal(options, $('#join-waitlist-modal'));
+
+        $(element).find('#join-waitlist-btn').on('click', function () {
+            $('#join-waitlist-modal').modal('openModal');
+        });
+    };
+});
