55e0f652f215b70f525070e66fbd46e470d1d27d
nishilelsner <nishil.s@elsner.com>
2026-05-06 12:36:04 +0530

    Added click event to submit button

--- app/code/Practice/Waitlist/view/frontend/web/js/waitlist_modal.js
+++ app/code/Practice/Waitlist/view/frontend/web/js/waitlist_modal.js
@@ -12,11 +12,31 @@ define([
             modalClass: 'waitlist-modal',
             buttons: []
         };
-
+        console.log(element);
         var popup = modal(options, $('#join-waitlist-modal'));
 
         $(element).find('#join-waitlist-btn').on('click', function () {
             $('#join-waitlist-modal').modal('openModal');
+        });
+
+        $('#waitlist-submit').on('click', function () {
+            alert('hello');
+            $.ajax({
+                url: '/waitlist/manage/submit',
+                type: 'POST',
+                data: {
+                    customer_name: $('#waitlist-name').val(),
+                    customer_email: $('#waitlist-email').val(),
+                    customer_phone: $('#waitlist-phone').val(),
+                    customer_comment: $('#waitlist-comment').val(),
+                    product_id: $('#waitlist-product-id').val()
+                },
+                success: function (response) {
+                    if (response.success) {
+                        alert(response.message);
+                    }
+                }
+            });
         });
     };
 });
